9e22ca73ab7d6b193eebfbdd09165f8b73188b8e
erick <law_erick@hotmail.com>
2016-03-17 16:02:06 -0500

    erick

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style type = "text/css"> 
+body { 
+background: url(public/imagenes/e.jpg) no-repeat; 
+background-size: 100% 100% 
+} 
+</style> 
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="shortcut icon"href="public/imagenes/5.png"/>
+	<title>One Piece</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="public/css/bootstrap.css">
+</head>
+<body>
+<body> 
+<div class="container">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">NetericK</a> <img src="public/imagenes/4.png" width="50">
+			</div>
+	
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a a href="http://reyanime.com/anime/ultimos/" target="http://reyanime.com/anime/ultimos/">Últimos Agregados</a></li>
+					<li><a a href="http://reyanime.com/anime/proximamente/" target="http://reyanime.com/anime/proximamente/">Proximamente</a></li>
+				</ul>
+				<nav class="navbar-form navbar-left">
+				<div id="search-box" class="main-search-box"> <form method="GET" action="http://www.animeyt.tv/busqueda" accept-charset="UTF-8"> <input placeholder="Mas Animes" name="terminos" type="text"><input style="display:none;" type="submit" value="Buscar"> </form> </div> 
+				</nav>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a a href="http://reyanime.com/anime/mas-vistos/" target="http://reyanime.com/anime/mas-vistos/">Populares</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">GENERO<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a a href="http://reyanime.com/genero/romance"  target="http://reyanime.com/genero/romance">Romance</a></li>
+							<li><a a href="http://reyanime.com/genero/comedia" target="http://reyanime.com/genero/comedia">Comedia</a></li>
+							<li><a a href="http://reyanime.com/genero/accion" target="http://reyanime.com/genero/accion">Acción</a></li>
+							<li><a a href="http://reyanime.com/genero/terror" target="http://reyanime.com/genero/terror">Terror</a></li>
+							<li><a a href="http://reyanime.com/genero/aventura" target="http://reyanime.com/genero/aventura">Aventura</a></li>
+							<li><a a href="http://reyanime.com/genero/magia" target="http://reyanime.com/genero/magia">Magia</a></li>
+							<li><a a href="http://reyanime.com/genero/ecchi" target="http://reyanime.com/genero/ecchi">ecchi</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+</div>
+	<!--slider-->
+	<center>
+	<div id="carousel-id" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img data-src="public/imagenes/1.jpg" alt="First slide" src="public/imagenes/1.jpg" width="1068">
+				<div class="container">
+					<div class="carousel-caption">
+						<p><a class="btn btn-lg btn-primary" a href="http://www.animeyt.tv/one-piece" target="http://www.animeyt.tv/one-piece" role="button">CAPITULOS ONE PIECE</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img data-src="public/imagenes/2.jpg" alt="Second slide" src="public/imagenes/2.jpg" width="1068">
+				<div class="container">
+					<div class="carousel-caption">
+						<p><a class="btn btn-lg btn-primary" a href="http://www.onepiecemangayanime.com/mangaonline/manga.html" target="http://www.onepiecemangayanime.com/mangaonline/manga.html" role="button">MANGA ONE PIECE</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item active">
+				<img data-src="public/imagenes/3.jpg" alt="Third slide" src="public/imagenes/3.jpg" width="1068">
+				<div class="container">
+					<div class="carousel-caption">
+						<p><a class="btn btn-lg btn-primary" a href="https://www.google.com.pe/search?q=imagenes+one+piece+hd&espv=2&biw=1366&bih=667&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjCw8-LsbTLAhUEFZAKHRukD8IQ_AUIBigB" target="https://www.google.com.pe/search?q=imagenes+one+piece+hd&espv=2&biw=1366&bih=667&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjCw8-LsbTLAhUEFZAKHRukD8IQ_AUIBigB" role="button">GALERIA ONE PECE</a></p>
+					</div>
+				</div>
+
+			</div>            
+		</div>
+		<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div>
+	</center>
+	<div class="slider-home__description"> <font color="pink"> <h1 class="slider-home__title">One Piece</h1></font>
+
+	<p class="slider-home__synopsis">
+	<font color="white"> Una historia épica de piratas, donde narra la historia de “Monkey D. Luffy” quien cuando tenia 7 años, comió accidentalmente una “Akuma no mi”(Fruta del diablo) la cual le convirtió en un hombre de goma. Por otra parte “Gol D. Roger” conocido como “El rey de los Piratas” quien fuera ejecutado por la Marine, habló antes de morir, acerca de su mayor tesoro,el “One Piece” escondido en la “Gran line”. Esta noticia desató la gran era de los piratas lanzando a incontables piratas a ese lugar, en busca del “One Piece” el tesoro perdido. Diez años después, Luffy inspirado en “Gol D. Roger” y un pirata de nombre Akagami no Shanks (Shanks el pelirrojo) se convierte en pirata deseando ser el próximo “Rey de los Piratas” y zarpar para conocer amigos y tener aventuras con ellos, teniendo como meta encontrar el “One Piece”.</font></p>
+
+	
+		<button >
+	<a href="http://www.animeyt.tv/ver/one-piece-732-sub-espanol" class="button desde-tablet" tabindex="0"> Ver último capítulo </a> </div>
+	</button>
+	
+	<img src="public/imagenes/6.jpg">
+<audio autobuffer autoloop loop autoplay="true">
+		<source src="public/imagenes/(One Piece AMV) -Centuries- (1).mp3">
+	</audio>
+	<img src="public/imagenes/7.png" width="500">
+<!-- Latest compiled and minified JavaScript -->
+<script src="public/js/jquery-1.12.1.js"></script>
+<script src="public/js/bootstrap.js">
+</script>
+</body>
+</html>
